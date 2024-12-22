50034be254d0f327faeb6f66f810038193b7c080
ShashithaU <udeshshashitha@gmail.com>
2024-12-22 08:51:35 +0530

    changed sidebar first commit

--- demo-project/src/App.jsx
+++ demo-project/src/App.jsx
@@ -1,20 +1,11 @@
+import SideBar from './components/SideBar';
+import './index.css';
 
-import './App.css'
-import Card from './components/Card'
-import Header from './components/header'
-import Sidebar from './components/SideBar'
-
-
-function App() {
-  
-
+export default function App() {
   return (
-    <>
-    <Header></Header>
- <Card></Card>
-  <Sidebar></Sidebar>
-    </>
-  )
-}
+<>
 
-export default App
+<SideBar></SideBar>
+</>
+  );
+}
