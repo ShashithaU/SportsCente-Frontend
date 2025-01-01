3b6de471a5bada11058827eb80c60ff79e33b4b0
harindu <manadul-ec20056@stu.stu.kln.ac.lk>
2025-01-01 12:45:10 +0530

    change sun parts

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,30 +1,9 @@
-import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import Header from "./Header";
-import { useState } from "react";
-import Catalog from "../features/catalog/Catalog";
 import ShippingDetailsPage from "./models/ShippingDetailspage/ShippingDetails"
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light';
 
-  const theme = createTheme({
-    palette:{
-      mode:paletteType,
-    }
-  })
-  function handleThemeChange(){
-    setDarkMode(!darkMode);
-  }
   return (
-    <ThemeProvider theme={theme}>
-    <CssBaseline/>
-    <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
-    <Container sx={{ paddingTop: "64px" }}>
-    <Catalog/>
-    </Container>
     <ShippingDetailsPage/>
-    </ThemeProvider>
   )
 }
 
