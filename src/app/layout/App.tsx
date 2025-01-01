9b13c299b31db134b0e562bd86ed1219c5a6245b
harindu <manadul-ec20056@stu.stu.kln.ac.lk>
2025-01-01 14:20:24 +0530

    added

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import { useState } from "react";
 import Catalog from "../features/catalog/Catalog";
 import ShippingDetailsPage from "./models/ShippingDetailspage/ShippingDetails"
+import Cart from "./models/Cart/Cart";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
