7b392d38859497533c00af20781453670d933e76
harindu <manadul-ec20056@stu.stu.kln.ac.lk>
2025-01-02 13:28:22 +0530

    adeed input fields

--- src/app/layout/models/PaymentDetails/PaymentDetails.tsx
+++ src/app/layout/models/PaymentDetails/PaymentDetails.tsx
@@ -4,8 +4,56 @@ import style from './PaymentDetails.module.css';
 
 const PaymentDetails = () => {
   return (
-    <div>
-      <h1></h1>
+    <div className={style.PaymentDetails}>
+        <div
+            className={style.nameInput}>
+
+            <div 
+                className={style.inputBlock}>
+                <label>Name</label>
+                <hr />
+                <input type="text" 
+                    placeholder='enter Name'
+                    required/>
+            </div>
+    
+            <div 
+                className={style.inputBlock}>
+                <label>Card Number</label>
+                <hr />
+                <input type="text" 
+                placeholder='enter card number'
+                required/>
+            </div>
+        </div>
+
+        <div
+            className={style.nameInput}>
+
+            <div 
+                className={style.inputBlock}>
+                <label>Ex Date</label>
+                <hr />
+                <input type="month"   
+                    placeholder='enter ex date'
+                    required/>
+            </div>
+    
+            <div 
+                className={style.inputBlock}>
+                <label>CVV</label>
+                <hr />
+                <input type="text"
+                placeholder='enter 3 digit code'
+                required/>
+            </div>
+        </div>
+
+        <div className={style.buttons}>
+
+            <button>Back</button>
+            <button>Next</button>
+        </div>
     </div>
   );
 };
